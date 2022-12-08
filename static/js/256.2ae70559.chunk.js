53e1ba6ce2789a9d19628dfb72ffad846db9118f
Psychedell <100085494+Psychedell@users.noreply.github.com>
2022-12-08 20:45:17 +0000

    Deploying to gh-pages from @ Psychedell/goit-react-hw-05-movies@49952f8df6b7df0d15d12009dcf714936dd9aee8 🚀

---
+++ static/js/256.2ae70559.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[256],{256:function(e,t,i){i.r(t);var n=i(731),s=i(184);t.default=function(){return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(n.rU,{to:"/",children:"Back to home page"}),(0,s.jsx)("div",{children:"This page is not found!"}),";"]})}}}]);
+//# sourceMappingURL=256.2ae70559.chunk.js.map
